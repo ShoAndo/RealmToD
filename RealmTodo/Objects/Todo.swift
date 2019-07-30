2bd30d8a0fd90b7f12c2c474501436f443fb46de
Sho Ando <spdtas0223@gmail.com>
2019-07-30 11:54:20 +0800

    no message

--- RealmTodo/Objects/Todo.swift
+++ RealmTodo/Objects/Todo.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2019 安藤奨. All rights reserved.
 //
 
-import Foundation
+import RealmSwift
+
+class Todo: Object{
+//     ID（連番）
+    @objc dynamic var id: Int = 0
+//     タイトル
+    @objc dynamic var title: String = ""
+//      日付
+    @objc dynamic var date:Date = Date()
+    
+    
+}
